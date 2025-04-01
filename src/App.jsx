ab246460d9283e9feedbd817192103165e0aff45
Manthan Bire <manthansharadbire@gmail.com>
2025-04-01 14:56:03 +0530

    bike cards added

--- src/App.jsx
+++ src/App.jsx
@@ -6,82 +6,85 @@ import shotgun650 from './assets/shotgun650.webp';
 import classic650 from './assets/classic650.webp';
 import BikeCard from './components/BikeCards';
 
+const BIKES = [
+  {
+    thumbnail: continentalgt650,
+    modelname: "Continental GT",
+    year: "2022",
+    engine: "650cc",
+    mileage: "17kmpl",
+  },
+  {
+    thumbnail: supermeteor650,
+    modelname: "Super Meteor",
+    year: "2023",
+    engine: "650cc",
+    mileage: "21kmpl"
+  },
+  {
+    thumbnail: interceptor650,
+    modelname: "Interceptor 650",
+    year: "2022",
+    engine: "650cc",
+    mileage: "19kmpl"
+  },
+  {
+    thumbnail: shotgun650,
+    modelname: "Shotgun 650",
+    year: "2022",
+    engine: "650cc",
+    mileage: "22kmpl"
+  },
+  {
+    thumbnail: classic650,
+    modelname: "Classic 650",
+    year: "2022",
+    engine: "650cc",
+    mileage: "24kmpl"
+  }
+];
+
 function App() {
 
-  const [bikeDetails, setBikeDetails] = useState([])
+  const [bikeDetails, setBikeDetails] = useState([]);
 
   const handleBikeDetails = (bike) => {
     if (bikeDetails.length >= 2) return;
-    setBikeDetails([...bikeDetails, bike])  
+    setBikeDetails([...bikeDetails, bike])
   };
 
-   
+
   return (
     <div>
       <div className='text-center bg-black'>
         <h1 className='text-2xl pt-3 pb-3 border-b shadow-md text-white'>Bike Dekho</h1>
       </div>
       <div className='flex justify-evenly m-5 cursor-pointer'>
-        <BikeCard
-          thumbnail={continentalgt650}
-          modelname="Continental GT"
-          onClick={() => handleBikeDetails({
-            modelname: "Continental GT",
-            year: "2022",
-            engine: "650cc",
-            mileage: "17kmpl"
+        {BIKES.map((obj, index) => {
+          return (
+            <BikeCard
+              key={index}
+              thumbnail={obj.thumbnail}
+              modelname={obj.modelname}
+              year= {obj.year}
+              engine={obj.engine}
+              mileage={obj.mileage}
+        onClick={handleBikeDetails}
+          />
+        );
+        })};
 
-          })}
-        />
-        <BikeCard
-          thumbnail={supermeteor650}
-          modelname="Super Meteor"
-          onClick={() => handleBikeDetails({
-            modelname: "Super Meteor",
-            year: "2022",
-            engine: "650cc",
-            mileage: "17kmpl"
-          })}
-        />
-        <BikeCard
-          thumbnail={interceptor650}
-          modelname="Interceptor 650"
-          onClick={() => handleBikeDetails({
-            modelname: "Interceptor 650",
-            year: "2022",
-            engine: "650cc",
-            mileage: "17kmpl"
-          })}
-        />
-        <BikeCard
-          thumbnail={shotgun650}
-          modelname="Shotgun 650"
-          onClick={() => handleBikeDetails({
-            modelname: "Shotgun 650",
-            year: "2022",
-            engine: "650cc",
-            mileage: "17kmpl"
-          })}
-        />
-        <BikeCard
-          thumbnail={classic650}
-          modelname="Classic 650"
-          onClick={() => handleBikeDetails({
-            modelname: "Classic 650",
-            year: "2022",
-            engine: "650cc",
-            mileage: "24kmpl"
-          })}
-        />
+
       </div>
       <div>
-        <div>
+        <div className='flex justify-between'>
           {bikeDetails.map((object, i) => (
-            <div key={i} className='bg-blue-500'>
-              <h1>{object.modelname}</h1>
-              <p>{object.year}</p>
-              <p>{object.engine}</p>
-              <p>{object.mileage}</p>
+            <div key={i} className='bg-blue-500 text-center m-5 p-4'>
+              <img src={object.thumbnail} alt={"Royal Enfield"} className='h-[350px]' />
+              <h1 className='text-2xl font-extrabold pt-3'>{object.modelname}</h1>
+              <p className='text-2xl'>{object.year}</p>
+              <p className='text-2xl'>{object.engine}</p>
+              <p className='text-2xl'>{object.mileage}</p>
             </div>
           ))}
         </div>
--- src/components/BikeCards.jsx
+++ src/components/BikeCards.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const BikeCard = ({ thumbnail, modelname, year, engine, mileage, onClick }) => {
   return (
     <div
-      className="bg-red-400 w-fit text-center border border-black shadow-md rounded-b-md
+      className="bg-red-400 w-fit text-center border border-black shadow-lg 
          transform transition duration-500 hover:scale-110"
       onClick={() => onClick({ thumbnail, modelname, year, engine, mileage })}
     >
       <img src={thumbnail} alt={modelname} className="h-[140px]" />
-      <h1 className="text-xl bg-black text-white p-2">{modelname}</h1>
+      <h1 className="text-xl bg-red-700 text-white p-2">{modelname}</h1>
     </div>
   );
 };
